e7924850d671c02ecc5015ed857b238de6120158
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-04-09 02:02:23 +0530

    Add run method

--- game.py
+++ game.py
@@ -1,6 +1,11 @@
+import pygame
+
 class Game:
     def __init__(self,settings):
         self.settings=settings
 
     def draw(self,display_surface):
         display_surface.fill(self.settings['bg_primary'])
+    
+    def run(self,display_surface):
+        self.draw(display_surface)
