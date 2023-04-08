f7e113ae6f4ad84c0cd33b89af0c1181ec5b2d5d
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-04-08 14:11:18 +0530

    Update main

--- main_self.py
+++ main_self.py
@@ -1,8 +1,14 @@
-import pygame
+import pygame,json,sys
+from game import Game
 
 class Main:
     def __init__(self):
         pygame.init()
+        f=open('settings.json')
+        self.settings=json.load(f)
+        f.close()
+
+        self.display_surface=pygame.display.set_mode((self.settings['window_width'],self.settings['window_height']))
         pygame.display.set_caption('Snake')
 
 if __name__=='__main__':
