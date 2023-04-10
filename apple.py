2351e7170819f2ef50486037eda601ff07fe58f2
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-04-10 10:19:36 +0530

    Update apple

--- apple.py
+++ apple.py
@@ -1,4 +1,8 @@
+import pygame,os
+
 class Apple:
     def __init__(self,settings,bodies):
         self.settings=settings
         self.colour=self.settings['apple_colour']
+        self.surface=pygame.image.load(os.path.join('graphics','apple.png')).convert_alpha()
+        self.surface=pygame.transform.scale(self.surface,(self.settings['pixels'],self.settings['pixels']))
