7739da47a2e7899d63160c3c1b33e129cf5f88ab
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-04-11 11:15:19 +0530

    add draw method

--- apple.py
+++ apple.py
@@ -6,3 +6,6 @@ class Apple:
         self.colour=self.settings['apple_colour']
         self.surface=pygame.image.load(os.path.join('graphics','apple.png')).convert_alpha()
         self.surface=pygame.transform.scale(self.surface,(self.settings['pixels'],self.settings['pixels']))
+
+    def draw(self,display_surface:pygame.Surface):
+        display_surface.blit(self.surface,self.rect)
