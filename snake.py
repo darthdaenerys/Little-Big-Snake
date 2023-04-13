4c97b72df7c6b9bddd2e12877d4a07f2eb7eac37
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-04-13 09:58:48 +0530

    Add Snake class

--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,random
 
 class SnakeBody:
     def __init__(self,pixels,xpos,ypos,colour):
@@ -9,3 +9,10 @@ class SnakeBody:
 
     def draw(self,display_surface):
         pygame.draw.rect(display_surface,self.colour,(self.xpos,self.ypos,self.pixels,self.pixels))
+
+class Snake:
+    def __init__(self,settings):
+        self.direction='None'
+        self.settings=settings
+        self.xhead=random.randrange(0,settings['window_width'],settings['pixels'])
+        self.yhead=random.randrange(0,settings['window_height'],settings['pixels'])
