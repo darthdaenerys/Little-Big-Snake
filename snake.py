bede4b00f9cc8edea71d4808136ff8f435452af0
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-04-13 09:58:19 +0530

    Add draw method

--- snake.py
+++ snake.py
@@ -1,6 +1,11 @@
+import pygame
+
 class SnakeBody:
     def __init__(self,pixels,xpos,ypos,colour):
         self.colour=colour
         self.xpos=xpos
         self.ypos=ypos
         self.pixels=pixels
+
+    def draw(self,display_surface):
+        pygame.draw.rect(display_surface,self.colour,(self.xpos,self.ypos,self.pixels,self.pixels))
