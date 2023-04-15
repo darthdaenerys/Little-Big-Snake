0e790c3e184e40719201fae74a747aa01d4ebce4
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-04-15 13:53:28 +0530

    Add render font

--- game.py
+++ game.py
@@ -5,6 +5,8 @@ class Game:
     def __init__(self,settings):
         self.settings=settings
         self.snake=Snake(settings)
+        self.score=0
+        self.font=pygame.font.SysFont('subatomic.ttf',50,bold=True)
 
     def draw(self,display_surface):
         display_surface.fill(self.settings['bg_primary'])
@@ -14,6 +16,11 @@ class Game:
                 if (row+col)%2==0:
                     pygame.draw.rect(display_surface,self.settings['bg_secondary'],(col*pixels,row*pixels,pixels,pixels))
     
+    def render_font(self,display_surface):
+        font_surface=self.font.render(f'Score: {self.score}',True,(255,255,255))
+        display_surface.blit(font_surface,(10,10))
+    
     def run(self,display_surface):
         self.draw(display_surface)
         self.snake.update(display_surface)
+        self.render_font(display_surface)
