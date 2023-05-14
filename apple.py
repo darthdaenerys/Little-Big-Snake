0ee2170c01dfa23ea271086bc9e6391343b5ae76
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-05-14 14:58:50 +0530

    Add rect attribute

--- apple.py
+++ apple.py
@@ -1,4 +1,4 @@
-import pygame,os
+import pygame,random,os
 
 class Apple:
     def __init__(self,settings,bodies):
@@ -6,6 +6,7 @@ class Apple:
         self.colour=self.settings['apple_colour']
         self.surface=pygame.image.load(os.path.join('graphics','apple.png')).convert_alpha()
         self.surface=pygame.transform.scale(self.surface,(self.settings['pixels'],self.settings['pixels']))
+        self.rect=self.surface.get_rect()
 
     def draw(self,display_surface:pygame.Surface):
         display_surface.blit(self.surface,self.rect)
