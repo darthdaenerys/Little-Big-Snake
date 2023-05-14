11275efaf06f1de9d35dc9119142ecc2c84ea494
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-05-14 14:59:41 +0530

    Update spawn method

--- apple.py
+++ apple.py
@@ -7,12 +7,17 @@ class Apple:
         self.surface=pygame.image.load(os.path.join('graphics','apple.png')).convert_alpha()
         self.surface=pygame.transform.scale(self.surface,(self.settings['pixels'],self.settings['pixels']))
         self.rect=self.surface.get_rect()
+        self.spawn(bodies)
     
     def spawn(self,bodies):
         while True:
             flag=True
             self.xpos=random.randrange(0,self.settings['window_width'],self.settings['pixels'])
             self.ypos=random.randrange(0,self.settings['window_height'],self.settings['pixels'])
+            for body in bodies:
+                if body.xpos==self.xpos and body.ypos==self.ypos:
+                    flag=False
+                    break
 
     def draw(self,display_surface:pygame.Surface):
         display_surface.blit(self.surface,self.rect)
