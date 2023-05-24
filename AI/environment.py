3b19be6d824ab4fc77f70c8c62975b28afd7558c
Sagnik Barman <sagnikskb0nyp437@gmail.com>
2023-05-24 10:56:53 +0530

    Add step method

--- AI/environment.py
+++ AI/environment.py
@@ -1,6 +1,7 @@
-import pygame
+import pygame,json,sys,os
 from AI.apple import Apple
 from AI.snake import Snake
+import pandas as pd
 
 class Environment:
     def __init__(self,display_surface,settings):
@@ -31,3 +32,47 @@ class Environment:
         self.display_surface.blit(label,(10,45))
         label=self.font.render(f'Best score: {best_score}',True,(0,0,0))
         self.display_surface.blit(label,(10,80))
+
+    def step(self,action,agent,games,neuralnetwork):
+        game_over=False
+        reward=0
+        self.frame_iteration+=1
+        self.clock.tick(120)
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_q):
+                self.settings['games']=games
+                dataframe=None
+                if self.settings['load_pretrained']:
+                    if os.path.exists(os.path.join('AI','data.csv')):
+                        dataframe=pd.read_csv(os.path.join('AI','data.csv'))
+                        dataframe=pd.concat([dataframe,pd.DataFrame(agent.score_dataframe)],axis=0)
+                        self.settings['best_score']=max(agent.best_score,self.settings['best_score'])
+                else:
+                    dataframe=pd.DataFrame(agent.score_dataframe)
+                    self.settings['best_score']=agent.best_score
+                dataframe.to_csv(os.path.join('AI','data.csv'),index=False)
+                neuralnetwork.save_model()
+                obj=json.dumps(self.settings,indent=4)
+                with open(os.path.join('AI','settings.json'),'w') as f:
+                    f.write(obj)
+                    f.close()
+                pygame.quit()
+                sys.exit()
+
+        self.draw()
+        self.snake.update(self.display_surface,action)
+        if (self.snake.wall_collision(self.snake.xhead,self.snake.yhead) or self.snake.body_collision(self.snake.xhead,self.snake.yhead) or agent.iterations>10*len(self.snake.bodies)+200):
+            reward=-10
+            game_over=True
+            self.snake.game_over_sound.play()
+            self.render_stats(agent.games,self.score,agent.best_score)
+            return game_over,reward,self.score
+        if self.snake.apple_collision(self.apple):
+            self.snake.add_body()
+            self.apple.spawn()
+            agent.iterations=0
+            self.score+=1
+            reward=10
+        self.apple.update(self.display_surface)
+        self.render_stats(agent.games,self.score,agent.best_score)
+        return game_over,reward,self.score
